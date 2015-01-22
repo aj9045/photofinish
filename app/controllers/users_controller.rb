c970e56fdbd858d7c920f0d2598f093ea5d9972a
aj9045 <identity_unveiled@hotmail.com>
2015-01-22 17:29:13 -0500

    add initial methods with their base functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def index
+    @users = User.all
   end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :about, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
