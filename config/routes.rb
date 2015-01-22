7c0ec55652fad3ab8d6097bb10a637b22108c1a2
aj9045 <identity_unveiled@hotmail.com>
2015-01-22 16:07:14 -0500

    add root, resources for users, and sesison login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  resources :users
+
+  get "/login" => "session#new"
+  post "/login" => "session#login"
+  get "/logout" => "session#logout"
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
