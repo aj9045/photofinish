58af895830db9c47b9ffacf08aa9944e0ab3b522
aj9045 <identity_unveiled@hotmail.com>
2015-01-24 15:37:22 -0500

    update user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates_presence_of :username, :email
+  validates_presence_of :password_digest, on: :create
+
+  validates_uniqueness_of :username, :email
+
+  # has_many :photos, :dependent => :destroy
 end
