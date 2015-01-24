913e18a0101f8a32247d087baf7a50da946c150a
aj9045 <identity_unveiled@hotmail.com>
2015-01-24 16:14:59 -0500

    update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,32 @@ require "rails_helper"
 
 describe User do
   describe "validations" do
-    it { should validate_presence_of :username }
-    it { should validate_presence_of :email }
-    it { should validate_presence_of :password }
-    it { should validate_presence_of :password_confirmation }
+    it "is has a valid factory" do
+      expect(build(:user)).to be_valid
+    end
+    it "is invalid without a username" do
+      user = build(:user, username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+    end
+    it "is invalid without an email" do
+      user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+    end
+    it "is invalid without a password[_digest]" do
+      user = build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    subject { build(:user) }
+
     it { should validate_uniqueness_of :username }
     it { should validate_uniqueness_of :email }
   end
 
-  describe "associations" do
-    it { should have_many :photos }
-  end
+  # describe "associations" do
+  #   it { should have_many :photos }
+  # end
 end
