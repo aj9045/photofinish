7a115d96016ddebc141f1ab938d2cb9094a7748e
aj9045 <identity_unveiled@hotmail.com>
2015-01-24 16:15:16 -0500

    fix redirect routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,37 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @user.id = session[:user_id]
+      session[:user_id] == nil
+      @user.destroy
+      redirect_to root_path
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :about, :password_confirmation)
+    params.require(:user).permit(:username, :email, :about, :password, :password_confirmation)
   end
 
   def find_user
