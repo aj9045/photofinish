89a16e73138b11adc2694358a35a5f09d147fe2d
aj9045 <identity_unveiled@hotmail.com>
2015-01-24 15:38:02 -0500

    didn't really change anything, just rewritten

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
     username "test_username"
-    sequence(:email) { |n| "name#{n}@dbc.com" }
+    sequence(:email) { |n| "test#{n}@dbc.com"}
     about "about me"
-    password "pswrd"
-    password_confirmation "pswrd"
+    password "test"
+    password_confirmation "test"
 
-      factory :invalid_user do
-        username nil
-      end
+    factory :invalid_user do
+      email nil
+    end
   end
 end
