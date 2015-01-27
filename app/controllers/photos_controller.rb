ffc63b6a3c682c127fb2cb22d0871dedd37f5c72
aj9045 <identity_unveiled@hotmail.com>
2015-01-27 18:13:05 -0500

    add content to methods

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,30 +2,49 @@ class PhotosController < ApplicationController
   before_action :find_photo, only: [:show, :edit, :update, :destroy]
 
   def index
+    @photos = Photo.all
   end
 
   def show
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+     @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @photo.update(photo_params)
+      redirect_to @photo
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @photo.user_id = session[:user_id]
+      @photo.destroy
+      redirect_to root_path
+    else
+      redirect_to @user
+    end
   end
 
   private
 
   def photo_params
-    params.require(:user).permit(:title, :description, :user_id)
+    params.require(:photo).permit(:title, :description, :user_id)
   end
 
   def find_photo
