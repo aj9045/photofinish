c402a3d9b7fefecf0a26e42b26e4e4c6c13a959c
aj9045 <identity_unveiled@hotmail.com>
2015-02-09 20:18:47 -0500

    add has_attached file and att content type valid for photo (paperclip)

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ActiveRecord::Base
   validates_presence_of :title, :user_id
+
+  has_attached_file :photo, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :user
 end
