442ad42dd8e337fe3133cdc5bcc007c5a251c0a8
aj9045 <identity_unveiled@hotmail.com>
2015-02-16 13:24:07 -0500

    add session controller spec tests. take a look at this again for login

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -3,18 +3,31 @@ require "rails_helper"
 describe SessionController do
   describe "Get#new" do
     it "renders login" do
+      get :new
+      expect(response).to render_template(:login)
     end
   end
 
-  describe "Post#login" do
-    it "sets session[:id] to user.id" do
+   describe "Post#login" do
+    before do
+      @user = create(:user)
+      session[:user_id] = @user.id
     end
-    it "redirects to root path" do
+    it "should set session with user id" do
+      post :login, user: @user.attributes
+      puts session[:user_id]
+      expect(session[:user_id]).to eq(@user.id)
+    end
+    it "should redirect to root_path" do
+      post :login, user: @user.attributes
+      expect(response).to redirect_to root_path
     end
   end
 
   describe "Delete#logout" do
     it "sets session[:user_id] back to nil" do
+      delete :logout
+      expect(session[:user_id]).to eq nil
     end
   end
 end
