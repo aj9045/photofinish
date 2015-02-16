27ff60c8a61b411ad039a15fd3776256dd75e112
aj9045 <identity_unveiled@hotmail.com>
2015-02-16 14:46:40 -0500

    permit image for user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :about, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :about, :password, :password_confirmation, :image)
   end
 
   def find_user
