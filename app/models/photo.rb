3c5ae8d2da02341362797d00af434dec9c62661c
aj9045 <identity_unveiled@hotmail.com>
2015-03-23 22:13:42 -0400

    add method to Photo model for whenever gem

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,17 @@ class Photo < ActiveRecord::Base
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
+
+  def self.update_datetime
+    photos = Photo.all
+    photos.each do |photo|
+      if photo.this_week == true && photo.last_week == false
+        photo.this_week = false
+        photo.last_week = true
+        photo.upload_week = (Date.today - 7).strftime('%B %d, %Y')
+      elsif photo.this_week == false && photo.last_week == true
+        photo.last_week == false
+      end
+    end
+  end
 end
