b08cf34b80d739a6b1797f4925198b82d142638a
aj9045 <identity_unveiled@hotmail.com>
2015-03-23 23:18:30 -0400

    change to update so will save to db

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,17 +5,25 @@ class Photo < ActiveRecord::Base
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
+  belongs_to :upload_tracker
+
+  def self.find_and_create_upload_week
+    upload_week = (Date.today - 7).strftime('%B %d, %Y')
+    UploadTracker.create(upload_week: upload_week)
+  end
 
   def self.update_datetime
     photos = Photo.all
     photos.each do |photo|
       if photo.this_week == true && photo.last_week == false
-        photo.this_week = false
-        photo.last_week = true
-        photo.upload_week = (Date.today - 7).strftime('%B %d, %Y')
+        photo.update(this_week: false)
+        photo.update(last_week: true)
+        photo.update(upload_tracker_id: UploadTracker.last.id)
       elsif photo.this_week == false && photo.last_week == true
-        photo.last_week == false
+        photo.update(last_week: false)
       end
     end
   end
+
+
 end
